1a4c11862d5ae8bf19c10464e86a5992d7251835
Mingyang Li <59183173+samlmy@users.noreply.github.com>
2022-03-11 19:16:15 -0500

    Add files via upload

---
+++ demonstration/loading_data.sql
@@ -0,0 +1,77 @@
+﻿create table tempTeam (
+	teamName text, 
+	league text
+);
+
+\copy tempTeam from team.csv delimiter ',' csv header;
+
+insert into team 
+select distinct teamName, league 
+from tempTeam 
+order by league;
+
+
+create table tempPlayer (
+	playerName text, 
+	teamName text
+);
+
+\copy tempPlayer from player.csv delimiter ',' csv header;
+
+insert into player 
+select distinct playerName, teamName 
+from tempPlayer 
+order by teamName;
+
+
+create table tempMatch (
+	gameID text,
+	length int not null,
+	patch float not null,
+	blue text not null,
+	red text not null,
+	result text not null
+);
+
+\copy tempMatch from match.csv delimiter ',' csv header;
+
+insert into Match 
+select distinct gameID, length, patch, blue, red, result
+from tempMatch 
+order by gameID;
+
+
+create table tempTimeline (
+	gameID text, 
+	teamName text, 
+	killsAt10 int,
+	killsAt15 int,
+	goldAt10 int,
+	goldAt15 int,
+	drakes int
+);
+
+\copy tempTimeline from timeline.csv delimiter ',' csv header;
+
+insert into TeamScoreboard 
+select gameID, teamName, killsAt10, killsAt15, goldAt10, goldAt15, drakes
+from tempTimeline 
+order by gameID;
+
+create table tempPlayerScoreboard (
+	gameID text, 
+	playerName text, 
+	position text, 
+	kills int, 
+	gold int, 
+	visionScore float
+);
+
+\copy tempPlayerScoreboard from scoreboard.csv delimiter ',' csv header;
+
+insert into PlayerScoreboard 
+select gameID, playerName, position, kills, gold, visionscore
+from tempPlayerScoreboard 
+order by gameID;
+
+drop table tempMatch, tempPlayer, tempPlayerScoreboard, tempTeam, tempTimeline;
---
+++ demonstration/schema.ddl.sql
@@ -0,0 +1,52 @@
+﻿drop schema if exists LolEsport cascade;
+create schema LolEsport;
+set search_path to LolEsport;
+
+create table Team ( 
+	teamName text,
+	league text not null,
+	primary key (teamName)
+);
+
+create table Player (
+	playerName text,
+	teamName text not null,
+	primary key (playerName, teamName),
+	foreign key (teamName) references Team(teamName)
+);
+
+create table Match (
+	gameID text,
+	length int not null,
+	patch text not null,
+	blue text not null,
+	red text not null,
+	result text not null,
+	primary key (gameID), 
+	foreign key (blue) references Team(teamName),
+	foreign key (red) references Team(teamName)
+);
+
+create table TeamScoreboard (
+	gameID text, 
+	teamName text,
+	killsAt10 int,
+	killsAt15 int,
+	goldAt10 int,
+	goldAt15 int,
+	drakes int,
+	primary key (gameID, teamName),
+	foreign key (gameID) references Match(gameID),
+	foreign key (teamName) references Team(teamName)
+);
+
+create table PlayerScoreboard (
+	gameID text, 
+	playerName text, 
+	position text not null, 
+	kills int, 
+	gold int, 
+	visionScore int,
+	primary key (gameID, playerName), 
+	foreign key (gameID) references Match(gameID)
+);
